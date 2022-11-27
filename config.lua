c29139565a9a6da1b734dea289423ef3ad4af771
OxyFW <118661082+OxyFW@users.noreply.github.com>
2022-11-27 22:01:16 +0000

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,118 @@
+Config = {}
+
+Config.GaragePedLocations = {
+    {coords = vector4(-1178.588, -891.9196, 13.799622, 302.58825)},
+}
+
+--if u are not using qb-target uncomment this.
+
+--[[Config.Locations = {
+    ["duty"] = {
+        [1] = vector3(85.73, 292.23, 110.21),
+    },    
+    ["tray1"] = {
+        [1] = vector3(87.79, 291.06, 110.18),
+    },
+    ["tray2"] = {
+        [1] = vector3(89.5, 288.16, -110.21),
+    },
+    ["tray3"] = {
+        [1] = vector3(89.72, 285.68, 110.28),
+    },
+    ["tray4"] = {
+        [1] = vector3(94.27, 285.63, -110.22),
+    },
+    ["pattycooker"] = {
+        [1] = vector3(97.15, 294.41, 110.12),
+    }, 
+    ["fryer"] = {
+        [1] = vector3(95.34, 294.93, 110.23),
+    }, 
+    ["drinks"] = {
+        [1] = vector3(96.59, 292.23, 110.23),
+        [2] = vector3(95.52, 289.55, 110.18),
+    },   
+    ["fridge"] = {
+        [1] = vector3(89.38, 296.93, 110.23),
+    },      
+    ["storage"] = {
+        [1] = vector3(93.43, 291.86, 109.96),
+    },
+    ["craftburger"] = {
+        [1] = vector3(93.13, 295.69, 110.27),
+    },    
+    ["cashregister"] = {
+        [1] = vector3(90.27, 290.17, 110.22),
+        [2] = vector3(90.57, 287.92, 110.24),
+    },   
+    ["garage"] = {
+        [1] = vector3(88.72, 310.29, -110.02),
+    },
+    ["stash"] = {
+        [1] = vector3(90.6, 293.01, 110.05),
+    }, 
+}]]
+Config.StashLoc = vector3(-1198.1, -893.21, 14.0)
+
+Config.Items = {
+label = "Shop",
+    slots = 7,
+    items = {
+        [1] = {
+            name = "burger-bun",
+            price = 6,
+            amount = 5000,
+            info = {},
+            type = "item",
+            slot = 1,
+        },
+        [2] = {
+            name = "burger-raw",
+            price = 10,
+            amount = 5000,
+            info = {},
+            type = "item",
+            slot = 2,
+        },
+        [3] = {
+            name = "burger-tomato",
+            price = 2,
+            amount = 5000,
+            info = {},
+            type = "item",
+            slot = 3,
+        },
+        [4] = {
+            name = "burger-lettuce",
+            price = 2,
+            amount = 5000,
+            info = {},
+            type = "item",
+            slot = 4,
+        },
+        [5] = {
+            name = "burger-potato",
+            price = 8,
+            amount = 5000,
+            info = {},
+            type = "item",
+            slot = 5,
+        },
+        [6] = {
+            name = "burger-mshakeformula",
+            price = 16,
+            amount = 5000,
+            info = {},
+            type = "item",
+            slot = 6,
+        },
+        [7] = {
+            name = "burger-sodasyrup",
+            price = 12,
+            amount = 5000,
+            info = {},
+            type = "item",
+            slot = 7,
+        },
+    }
+}
